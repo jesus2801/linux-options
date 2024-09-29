6a80a5fb69cd246b64f87b026b05c12275d5604a
jesus2801 <jesusd280147@gmail.com>
2024-09-29 08:37:21 -0500

    battery script updated: now it also checks the status

--- terminal/scripts/battery.sh
+++ terminal/scripts/battery.sh
@@ -1,5 +1,6 @@
 percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "percentage:.*" | grep -oE "[0-9]+")
+state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state:." | grep -oE "\w+$")
 
-if [ $percentage -lt 20 ]; then
+if [ $percentage -lt 20 && $state = "discharging" ]; then
   notify-send -u critical "CHARGE BATTERY" "Hey! You should charge the battery right now, it is too low dude :("
 fi
