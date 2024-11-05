766825535031a6e2084a3d71ea9d7809623a0e1d
jesus2801 <jesusd280147@gmail.com>
2024-11-04 19:40:13 -0500

    lazyvim configuration updated

--- lazyvim/keymaps.lua
+++ lazyvim/keymaps.lua
@@ -3,9 +3,9 @@
 
 vim.keymap.set({ "i" }, "kk", "<ESC>")
 
-vim.keymap.del("n", "<C-k>")
-vim.keymap.del("n", "<C-j>")
-vim.keymap.set("n", "<C-k>", "8k")
-vim.keymap.set("n", "<C-j>", "8j")
+--vim.keymap.del("n", "<C-k>")
+--vim.keymap.del("n", "<C-j>")
+--vim.keymap.set("n", "<C-k>", "8k")
+--vim.keymap.set("n", "<C-j>", "8j")
 
 vim.keymap.set("n", "cc", ":bd<CR>")
--- lazyvim/lazy.lua
+++ lazyvim/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.formatting.black" },
     {
       "nvim-lua/plenary.nvim",
     },
