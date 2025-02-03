acf09e9c68567892828b66dd1b3c7ba772f9d436
jesus2801 <jesusd280147@gmail.com>
2025-02-03 13:36:28 -0500

    keymaps changed for some plugins

--- lazyvim/keymaps.lua
+++ lazyvim/keymaps.lua
@@ -1,5 +1,11 @@
 -- vim.keymap.set('n', '<leader>ex', function)
 -- vim.keymap.del('n', '<Leader>ex1')
+local live_rename = require("live-rename")
+vim.keymap.set("n", "<leader>r", live_rename.rename, { desc = "LSP rename" })
+
+vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
+vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
+vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
 
 vim.keymap.set({ "i" }, "kk", "<ESC>")
 
